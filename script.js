98037bd9a99b6ca7c2f86520738921b6f3abd2b2
Eder Canada <canada.eder@gmail.com>
2021-09-29 10:53:41 -0700

    login

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 let movements = account1.movements;
 
-function dispayMovements(movements) {
+function displayMovements(movements) {
   containerMovements.innerHTML = '';
   movements.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
@@ -72,14 +72,33 @@ function dispayMovements(movements) {
       <div class="movements__type movements__type--${type}"> ${
       i + 1
     } ${type} </div>
-      <div class="movements__value">${mov}</div>
+      <div class="movements__value">$${mov}</div>
     </div>
     `;
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 }
 
-dispayMovements(movements);
+function calcDisplaySummary(account) {
+  const income = account.movements
+    .filter((mov) => mov > 0)
+    .reduce((total, num) => total + num);
+  labelSumIn.textContent = `$${income}`;
+
+  let out = account.movements
+    .filter((mov) => mov < 0)
+    .reduce((total, num) => total + num);
+
+  labelSumOut.textContent = `$${Math.abs(out)}`;
+
+  const interest = account.movements
+    .filter((mov) => mov > 0)
+    .map((deposit) => (deposit * account.interestRate) / 100)
+    .filter((deposit) => deposit > 1)
+    .reduce((total, num) => total + num);
+
+  labelSumInterest.textContent = `$${interest.toFixed(2)}`;
+}
 
 function createUsernames(accs) {
   accs.forEach(function (acc) {
@@ -97,7 +116,7 @@ function calcDisplayBalance(movements) {
   const balance = movements.reduce((total, num) => {
     return total + num;
   });
-  labelBalance.textContent = `${balance}€`;
+  labelBalance.textContent = `$${balance}`;
 }
 calcDisplayBalance(account1.movements);
 
@@ -114,7 +133,6 @@ const balance = movements.reduce((total, num) => {
 });
 
 // Maximum value
-
 const max = movements.reduce((largestNum, num) => {
   if (largestNum > num) {
     largestNum = largestNum;
@@ -124,4 +142,37 @@ const max = movements.reduce((largestNum, num) => {
   return largestNum;
 });
 
-console.log(max);
+let firstWithdrawal = movements.find((mov) => mov < 0);
+console.log(firstWithdrawal);
+
+const account = accounts.find((acc) => acc.owner === 'Jessica Davis');
+console.log(account);
+
+// Event Handler
+let currentAccount;
+btnLogin.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    (acc) => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and messages
+    labelWelcome.textContent = `Welcome back ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    // Display Movements
+    containerApp.style.opacity = 100;
+    inputLoginUsername.value = inputLoginPin.value = ''
+    inputLoginPin.blur()
+
+    // Display Balance
+    displayMovements(currentAccount.movements);
+    // Display Summary
+    calcDisplayBalance(currentAccount.movements);
+    // Display Summary
+    calcDisplaySummary(currentAccount);
+  }
+});
